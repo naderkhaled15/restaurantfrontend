722a21d76709f1efd04cfa183e5dc0c8137e4361
naderkhaled15 <nader.khaled.dev@gmail.com>
2022-11-01 14:19:12 +0200

    initialize project

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Frienze Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  startMenu: ["Focaccia", "Bruschetta", "Garlic", "Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+};
