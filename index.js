51303562b2f5d25d104773d89d3ac69e5a62568e
naderkhaled15 <nader.khaled.dev@gmail.com>
2022-11-01 14:54:37 +0200

    change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 const restaurant = {
-  name: "Classico Italiano Medium",
+  name: "Classico  Medium",
   location: "Via Angelo Tavanti 23, Frienze Italy",
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   startMenu: ["Focaccia", "Bruschetta", "Garlic", "Bread", "Caprese Salad"],
